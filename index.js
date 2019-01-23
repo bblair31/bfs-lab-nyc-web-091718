1d59271b7167d7a7473957b79ea120f7b5884997
Brennan Blair <bblair31@users.noreply.github.com>
2019-01-22 19:48:49 -0500

    Wrote bfs function with helper functions

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 function bfs(rootNode, vertices, edges){
+  rootNode.distance = 0
+  let discovered = [rootNode]
+  let discoverOrder = [rootNode]
 
+  while(discovered.length != 0){
+    let currentNode = discovered.shift()
+    let adjacentNodes = findAdjacent(currentNode.name, vertices, edges)
+    adjacentNodes
+    discoverOrder = discoverOrder.concat(adjacentNodes)
+
+    markDistanceAndPredecessor(currentNode, adjacentNodes)
+    discovered = discovered.concat(adjacentNodes)
+  }
+  return discoverOrder
 }
 
 function findAdjacent(nodeName, vertices, edges) {
   let sharedEdges = edges.filter(edge => edge.includes(nodeName))
   let adjacent = []
-  
+
   sharedEdges.forEach(edge => {
     let foundVertex
 
@@ -14,28 +27,14 @@ function findAdjacent(nodeName, vertices, edges) {
     } else if (edge[1] === nodeName) {
       foundVertex = vertices.find(vertex => vertex.name === edge[0])
     }
-    foundVertex.distance !== 0 ? adjacent.push(foundVertex) : null
+    foundVertex.distance === null ? adjacent.push(foundVertex) : null
   })
   return adjacent
 }
 
-
-// edges = [
-//   ['14th&6th', '23rd&6th'],
-//   ['23rd&6th', '34th&6th'],
-//   ['34th&6th', '28th&Bwy'],
-//   ['28th&Bwy', '23rd&Bwy'],
-//   ['23rd&Bwy', '14th&Lex'],
-//   ['14th&Lex', '23rd&Lex']
-// ]
-//
-// vertices = [
-//   {name: '34th&6th', distance: null, predecessor: null},
-//   {name: '23rd&6th', distance: null, predecessor: null},
-//   {name: '28th&Bwy', distance: null, predecessor: null},
-//
-//   {name: '14th&6th', distance: null, predecessor: null},
-//   {name: '23rd&Bwy', distance: null, predecessor: null},
-//   {name: '14th&Lex', distance: null, predecessor: null},
-//   {name: '23rd&Lex', distance: null, predecessor: null}
-// ]
+function markDistanceAndPredecessor(predecessor, adjacentNodes) {
+  adjacentNodes.map(node => {
+    node.distance = predecessor.distance + 1
+    node.predecessor = predecessor
+  })
+}
